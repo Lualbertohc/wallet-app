650f7c1e7e8fbd4c15bb99d3011857c77b079611
Luis Alberto H. Cardoso <luis.alberto3556@gmail.com>
2022-10-04 22:37:58 -0300

    segundo requisito

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import WalletForm from './components/WalletForm';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path="/" component={ WalletForm } />
         </Switch>
