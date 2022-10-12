518b7d38cd5c92132eae496812d998c9ad71d97c
Luis Alberto H. Cardoso <luis.alberto3556@gmail.com>
2022-10-12 14:11:38 -0300

    requisito 5 refatorado

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -4,36 +4,25 @@ import { renderWithRouterAndRedux } from './helpers/renderWith';
 import App from '../App';
 
 describe('Testa a página Login', () => {
-//   const E = screen.getByTestId('email-input');
-//   const P = screen.getByTestId('password-input');
-//   const B = screen.getByTestId('btn');
+//   const EMAIL = screen.getByTestId('email-input');
+//   const PASSWORD = screen.getByTestId('password-input');
+//   const BTN = screen.getByTestId('btn');
 
-  it('Verifica se existem os inputs de email e password', () => {
-    renderWithRouterAndRedux(<App />);
-    const E = screen.getByTestId('email-input');
-    const P = screen.getByTestId('password-input');
-    expect(E).toBeDefined();
-    expect(P).toBeDefined();
-  });
-
-  it('Verifica se o button Entrar está desabilitado com inputs incompletos', () => {
-    renderWithRouterAndRedux(<App />);
-    const E = screen.getByTestId('email-input');
-    const P = screen.getByTestId('password-input');
-    const B = screen.getByTestId('btn');
-    userEvent.type(E, 'teste@teste.com');
-    userEvent.type(P, '');
-    expect(B).toBeDisabled();
-  });
-
-  it('Verifica se clicar no button Entrar redireciona para "/carteira"', () => {
+  it('Verificar inputs', () => {
     const { history } = renderWithRouterAndRedux(<App />);
-    const E = screen.getByTestId('email-input');
-    const P = screen.getByTestId('password-input');
-    const B = screen.getByTestId('btn');
-    userEvent.type(E, 'trybe@trybe.com');
-    userEvent.type(P, 'trybe');
-    userEvent.click(B);
+    const EMAIL = screen.getByTestId('email-input');
+    const PASSWORD = screen.getByTestId('password-input');
+    const BTN = screen.getByTestId('btn');
+    expect(EMAIL).toBeDefined();
+    expect(PASSWORD).toBeDefined();
+
+    userEvent.type(EMAIL, 'teste@teste.com');
+    userEvent.type(PASSWORD, '');
+    expect(BTN).toBeDisabled();
+
+    userEvent.type(EMAIL, 'trybe@trybe.com');
+    userEvent.type(PASSWORD, 'trybe');
+    userEvent.click(BTN);
     waitFor(() => {
       expect(history.location.pathname).toBe('/carteira');
     });
