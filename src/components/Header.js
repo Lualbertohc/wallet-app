e8f043812d3a1a7c863565ce211807062932dd8f
Luis Alberto Henriques Cardoso <lualbertohc@pop-os.localdomain>
2022-12-29 01:32:22 -0300

    barra de pesquisa no header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -5,8 +7,51 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import InputBase from '@mui/material/InputBase';
 import React, { useContext } from 'react';
 import context from '../context/context';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 function Header() {
   const { data } = useContext(context);
@@ -21,25 +66,29 @@ function Header() {
             size="large"
             edge="start"
             color="inherit"
-            aria-label="menu"
+            aria-label="open drawer"
             sx={ { mr: 2 } }
           >
             <MenuIcon />
           </IconButton>
           <Typography
             variant="h6"
+            noWrap
             component="div"
-            sx={ {
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            } }
+            sx={ { display: { xs: 'none', sm: 'block' } } }
           >
             WALLET
           </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Filtro..."
+              inputProps={ { 'aria-label': 'search' } }
+            />
+          </Search>
+          <Box sx={ { flexGrow: 1 } } />
           <Button color="inherit">{ `Email: ${email}` }</Button>
           <Button color="inherit">BRL</Button>
         </Toolbar>
